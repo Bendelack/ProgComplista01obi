33e6398d6ca83632e8ad0b62828180796cf9afb4
Rick Hill <rickk1010@gmail.com>
2023-04-23 22:07:25 -0300

    Questao cores.py atualizada.

--- cores.py
+++ cores.py
@@ -1,7 +1,7 @@
 r,g,b = map(int,input().split())
 
 if r >= 0 and r <= 255 and g >= 0 and g <= 255 and b >= 0 and b <= 255:
-    if b - g == g - r:
+    if b - g == g - r and b - g != 0 and g - r != 0:
         print('S')
     elif (r == 255 and g == 0 and b == 0):
         print('S')
