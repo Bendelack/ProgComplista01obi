f60841aa1ba8d4a875d30187e81804d1747d1b9f
Rick Hill <rickk1010@gmail.com>
2023-04-23 21:27:09 -0300

    Atualizando questao album

--- album.py
+++ album.py
@@ -1,16 +1,7 @@
-largpag, altpag = map(int,input().split())# 7 5
-l1, a1     = map(int,input().split())# 3 4
-l2, a2     = map(int,input().split())# 3 4
+largpag, altpag = map(int,input().split())
+l1, a1     = map(int,input().split())
+l2, a2     = map(int,input().split())
 
-# 13 8
-# 4 9
-# 6 5
-
-#l1=l2;l1=a2;a1l2;a1=a2*.
-# if l1 > largpag or l2 > largpag or a1 > largpag or a2 > largpag:
-#     print('N')
-# else:
-# if a1 + a2 > altpag:
 if a1 + a2 <= altpag and l1 <= largpag and l2 <= largpag:
     resposta = 'S'
 elif a1 + l2 <= altpag and a2 <= largpag and l1 <= largpag:
@@ -29,15 +20,5 @@ elif l1 + l2 <= largpag and a1 <= altpag and a2 <= altpag:
     resposta = 'S'
 else:
     resposta = 'N'
-    #a1 + l2 <= largpag or (a2 + l1 <= altpag or a2 + l1 <= largpag) or (l1 + l2 <= altpag or l1 + l2 <= largpag):
-#     print('S')
-# elif (a2 + l1 <= altpag or a2 + l1 <= largpag) and (a1 + l2 <= largpag or a1 + l2 <= altpag):
-#     print('S')
-# elif (l1 + l2 <= altpag or l1 + l2 <= largpag) and (l2 + l1 <= largpag or l2 + l1 <= altpag):
-#     print('S')
-# else:
-#     print('N')
-# else:
-#     print('S')
 
 print(resposta)
