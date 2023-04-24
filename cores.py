3545deab51411110784cc8ba4715250b6dae5faa
Rick Hill <rickk1010@gmail.com>
2023-04-23 23:58:23 -0300

    Questao cores finalizada

--- cores.py
+++ cores.py
@@ -1,17 +1,19 @@
 r,g,b = map(int,input().split())
+lista = [r, g, b]
+lista.sort()
 
-if r >= 0 and r <= 255 and g >= 0 and g <= 255 and b >= 0 and b <= 255:
-    if b - g == g - r and b - g != 0 and g - r != 0:
+if lista[0] >= 0 and lista[0] <= 255 and lista[1] >= 0 and lista[1] <= 255 and lista[2] >= 0 and lista[2] <= 255:
+    if lista[2] - lista[1] == lista[1] - lista[0] and lista[2] - lista[1] != 0 and lista[1] - lista[0] != 0:
         print('S')
-    elif (r == 255 and g == 0 and b == 0):
+    elif (lista[0] == 255 and lista[1] == 0 and lista[2] == 0):
         print('S')
-    elif (g == 255 and r == 0 and b == 0):
+    elif (lista[1] == 255 and lista[0] == 0 and lista[2] == 0):
         print('S')
-    elif (b == 255 and g == 0 and r == 0):
+    elif (lista[2] == 255 and lista[1] == 0 and lista[0] == 0):
         print('S')
-    elif (r == 0   and g == 0 and b == 0):
+    elif (lista[0] == 0   and lista[1] == 0 and lista[2] == 0):
         print('S')
-    elif (r == 255 and g == 255 and b == 255):
+    elif (lista[0] == 255 and lista[1] == 255 and lista[2] == 255):
         print('S')
     else:
         print('N')
